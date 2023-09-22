edb13c1ab81ce4d3577237ae0f6f3f63e0f12887
Omotayo <kehindeomotayo11@gmail.com>
2023-09-22 13:00:49 +0100

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,26 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  const linkStyle = {
+    marginRight: "20px", // Add margin between the links
+  };
+
+  return (
+    <div>
+      <NavLink to="/" style={linkStyle}>
+        Home
+      </NavLink>
+      <NavLink to="/movies" style={linkStyle}>
+        Movies
+      </NavLink>
+      <NavLink to="/directors" style={linkStyle}>
+        Directors
+      </NavLink>
+      <NavLink to="/actors" style={linkStyle}>
+        Actors
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
